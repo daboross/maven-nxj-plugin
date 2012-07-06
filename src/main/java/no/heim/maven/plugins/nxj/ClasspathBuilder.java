bbc8f456b2f2710bfa8797f3344128f58c59603d
Felix Müller <felix.mueller.berlin@googlemail.com>
2012-07-06 23:03:58 +0200

    fixed tabs and spaces

--- src/main/java/no/heim/maven/plugins/nxj/ClasspathBuilder.java
+++ src/main/java/no/heim/maven/plugins/nxj/ClasspathBuilder.java
@@ -7,51 +7,51 @@ import org.apache.maven.model.Dependency;
 
 public class ClasspathBuilder {
 
-	private final String rootPath;
-	private final Collection<Dependency> dependencies;
+    private final String rootPath;
+    private final Collection<Dependency> dependencies;
 
-	public ClasspathBuilder(String rootPath, Collection<Dependency> dependencies) {
-		this.rootPath = rootPath;
-		this.dependencies = dependencies;
-	}
+    public ClasspathBuilder(String rootPath, Collection<Dependency> dependencies) {
+        this.rootPath = rootPath;
+        this.dependencies = dependencies;
+    }
 
-	public String build() {
-		final StringBuilder path = new StringBuilder();
-		for (Dependency d : dependencies) {
-			if (d.getScope().equalsIgnoreCase("compile")) {
-				appendFile(path, rootPath);
-				for (String groupPart : d.getGroupId().split("\\.")) {
-					appendFile(path, groupPart);
-				}
-				appendFile(path, d.getArtifactId());
-				appendFile(path, d.getVersion());
-				appendArtifactFilename(path, d);
-				path.append(File.pathSeparator);
-			}
-		}
-		return cutFilePathSeparatorAtEnd(path);
-	}
+    public String build() {
+        final StringBuilder path = new StringBuilder();
+        for (Dependency d : dependencies) {
+            if (d.getScope().equalsIgnoreCase("compile")) {
+                appendFile(path, rootPath);
+                for (String groupPart : d.getGroupId().split("\\.")) {
+                    appendFile(path, groupPart);
+                }
+                appendFile(path, d.getArtifactId());
+                appendFile(path, d.getVersion());
+                appendArtifactFilename(path, d);
+                path.append(File.pathSeparator);
+            }
+        }
+        return cutFilePathSeparatorAtEnd(path);
+    }
 
-	private void appendFile(StringBuilder path, String file) {
-		path.append(file);
-		if (!file.endsWith(File.separator)) {
-			path.append(File.separator);
-		}
-	}
+    private void appendFile(StringBuilder path, String file) {
+        path.append(file);
+        if (!file.endsWith(File.separator)) {
+            path.append(File.separator);
+        }
+    }
 
-	private void appendArtifactFilename(StringBuilder path, Dependency dependency) {
-		path.append(dependency.getArtifactId());
-		path.append('-');
-		path.append(dependency.getVersion());
-		path.append('.');
-		path.append(dependency.getType());
-	}
+    private void appendArtifactFilename(StringBuilder path, Dependency dependency) {
+        path.append(dependency.getArtifactId());
+        path.append('-');
+        path.append(dependency.getVersion());
+        path.append('.');
+        path.append(dependency.getType());
+    }
 
-	private String cutFilePathSeparatorAtEnd(final StringBuilder path) {
-		if (path.toString().endsWith(File.pathSeparator)) {
-			return path.toString().substring(0, path.length() - File.pathSeparator.length());
-		} else {
-			return path.toString();
-		}
-	}
+    private String cutFilePathSeparatorAtEnd(final StringBuilder path) {
+        if (path.toString().endsWith(File.pathSeparator)) {
+            return path.toString().substring(0, path.length() - File.pathSeparator.length());
+        } else {
+            return path.toString();
+        }
+    }
 }
--- src/main/java/no/heim/maven/plugins/nxj/LinkMojo.java
+++ src/main/java/no/heim/maven/plugins/nxj/LinkMojo.java
@@ -12,14 +12,15 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
 
 /**
- * Echos an object string to the output screen.
+ * Links all compiled classes to an executable for the NXJ platform.
+ * 
  * @goal link
  * @requiresProject true
  * @execute phase="compile"
  */
 public class LinkMojo extends AbstractMojo {
 
-	/**
+    /**
      * @parameter expression="${project}"
      * @required
      * @readonly
@@ -28,38 +29,42 @@ public class LinkMojo extends AbstractMojo {
 
     /**
      * Location of lejos-classes.
+     * 
      * @parameter expression="${bootClassPath}"
      */
     private Object bootClassPath;
-    
+
     /**
      * Name of main class
+     * 
      * @parameter expression="${mainClass}"
      */
     private Object mainClass;
-    
+
     /**
      * Name of output application
+     * 
      * @parameter expression="${applicationName}
      */
     private Object applicationName;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
-    	final ClasspathBuilder cpBuilder = new ClasspathBuilder(
-    			System.getProperty("user.home") + File.separator + ".m2" + File.separator + "repository",
-    			project.getDependencies());
-    	try {
-			new NXJLink().run(
-					new String[] { "-bp", bootClassPath.toString(),
-								   "-cp", "target" + File.separator +"classes" + File.pathSeparator + cpBuilder.build(),
-								   "-wo", "LE", "-o", "target" + File.separator + applicationName.toString(),
-								   mainClass.toString() });
-		} catch (TinyVMException e) {
-			getLog().error("Could not perform linking", e);
-			throw new MojoFailureException(e, "Error", "");
-		} catch (IOException e) {
-			getLog().error("Could not perform linking", e);
-			throw new MojoFailureException(e, "Error", "");
-		}
+        final String localRepository = System.getProperty("user.home")
+                + File.separator + ".m2" + File.separator + "repository";
+        final ClasspathBuilder cpBuilder = new ClasspathBuilder(localRepository, project.getDependencies());
+        try {
+            new NXJLink().run(new String[] {
+                    "-bp", bootClassPath.toString(),
+                    "-cp", "target" + File.separator + "classes" + File.pathSeparator + cpBuilder.build(),
+                    "-wo", "LE",
+                    "-o", "target" + File.separator + applicationName.toString(),
+                    mainClass.toString() });
+        } catch (TinyVMException e) {
+            getLog().error("Could not perform linking", e);
+            throw new MojoFailureException(e, "Error", "");
+        } catch (IOException e) {
+            getLog().error("Could not perform linking", e);
+            throw new MojoFailureException(e, "Error", "");
+        }
     }
 }
