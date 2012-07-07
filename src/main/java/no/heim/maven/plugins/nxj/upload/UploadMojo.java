b232100414a070c25a368a5f9d59e02dc83b5492
Felix Müller <felix.mueller.berlin@googlemail.com>
2012-07-07 10:06:04 +0200

    added goal for uploading an executable to nxt robot

---
+++ src/main/java/no/heim/maven/plugins/nxj/upload/UploadMojo.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2012 Felix Müller <felix.mueller.berlin@googlemail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.heim.maven.plugins.nxj.upload;
+
+import java.io.File;
+import java.io.IOException;
+
+import lejos.pc.comm.NXTCommFactory;
+import lejos.pc.tools.NXTNotFoundException;
+import lejos.pc.tools.Upload;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Uploads a given NXJ executable file to a NXT robot.
+ *
+ * @goal upload
+ * @author Felix Müller
+ */
+public class UploadMojo extends AbstractMojo {
+
+    /**
+     * Name of the NXT
+     * 
+     * @parameter expression="${nxtName}
+     */
+    private String nxtName;
+
+    /**
+     * Mac address of the NXT
+     * 
+     * @parameter expression="${nxtAddress}
+     */
+    private String address;
+
+    /**
+     * The protocol to use for transmission (either "usb" or "bluetooth")
+     * 
+     * @parameter expression="${protocol}
+     */
+    private String protocol;
+
+    /**
+     * The nxj executable file
+     *
+     * @parameter expression="${executable}
+     */
+    private String executable;
+
+    /**
+     * The filename of the nxj executable file at the robot
+     *
+     * @parameter expression="${nxtFilename}
+     */
+    private String nxtFilename;
+
+    /**
+     * If true the uploaded executable starts immediately
+     *
+     * @parameter expression="${runImmediately}
+     */
+    private boolean shouldImmediatelyRun;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try {
+
+            final boolean isUsb = protocol.equalsIgnoreCase("usb");
+            new Upload().upload(nxtName,
+                    address,
+                    (isUsb ? NXTCommFactory.USB : NXTCommFactory.BLUETOOTH),
+                    new File(executable),
+                    nxtFilename,
+                    shouldImmediatelyRun);
+
+        } catch (NXTNotFoundException e) {
+            getLog().error("Could not perform upload", e);
+            throw new MojoFailureException(e, "NXT not found", "The given NXT was not found");
+        } catch (IOException e) {
+            getLog().error("Could not perform upload", e);
+            throw new MojoFailureException(e, "IO error at upload", "IO error at upload to the NXT robot");
+        }
+    }
+}
